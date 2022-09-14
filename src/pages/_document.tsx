7cd89c63779c5e061e8a2dab9d7a80deaf37fc76
riolly <alberto.riolly@gmail.com>
2022-09-14 13:23:49 +0700

    :lipstick: style: fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,17 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link
+						rel='preconnect'
+						href='https://fonts.gstatic.com'
+						crossOrigin=''
+					/>
+					<link
+						href='https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400&family=Poppins:ital,wght@0,300;0,500;0,700;1,300;1,500;1,700&display=swap'
+						rel='stylesheet'
+					/>
+
 					<link
 						rel='apple-touch-icon'
 						sizes='180x180'
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				heading: ['Poppins', 'sans-serif'],
+				body: ['Inter', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }

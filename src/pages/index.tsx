af02d0615f5d1f9ce5f8da2cb9268d403e331d3d
riolly <alberto.riolly@gmail.com>
2022-09-20 15:54:23 +0700

    :lipstick: style: responsive homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,37 +13,40 @@ const Home: NextPage = () => {
 			</Head>
 
 			<main className=''>
-				<section className='h-screen bg-gradient-to-br from-gray-900 via-purple-900  to-violet-600'>
-					<div className='sbg-red-300/[0.5] container absolute top-1/2 left-1/2 max-w-screen-xl -translate-y-[60%]  -translate-x-1/2'>
-						<div className='grid grid-cols-12'>
-							<div className='sbg-blue-300/[0.5] col-span-7 flex flex-col gap-4 self-center px-16 text-white'>
-								<h1 className='text-5xl'>
+				<section className='relative h-screen bg-gradient-to-br from-gray-900  via-purple-900 to-violet-600'>
+					<div className='sbg-red-300/[0.5] absolute top-1/2 -translate-y-[50%] md:container md:left-1/2 md:max-w-screen-xl md:-translate-x-1/2 md:-translate-y-[60%]'>
+						<div className='mx-4 grid grid-cols-12 md:mx-0'>
+							<div className='sbg-blue-300/[0.5] relative col-span-12 flex flex-col gap-2 self-center overflow-hidden text-white md:col-span-7 md:ml-16 md:gap-4 lg:px-16'>
+								<h1 className='text-3xl md:text-5xl'>
 									Term and condition <br />
 									made
 									<span className='italic'> transparent</span>
 								</h1>
-								<h2 className='text-2xl'>
+								<TNC className='mx-auto my-8 w-72 sm:my-16 md:absolute md:left-[1000%]' />
+								<h2 className='text-lg sm:text-center sm:text-xl md:text-left md:text-2xl'>
 									Search app. Read highlights. Decide!
 								</h2>
-								<p>
-									No more getting abused by irresponsible companies who force us{' '}
-									<br /> to agree on something we don&apos;t want. <br />
-									With the power of the community, transparency give you the
-									concise points, summarizing the verbose and confusing terms
-									that company provide.
+								<p className='ml-4 text-sm sm:mx-4 sm:text-justify sm:text-base md:ml-0'>
+									<span className='hidden sm:inline'>
+										No more getting abused by irresponsible companies who force
+										us to agree on something we don&apos;t want. <br />
+									</span>
+									With the community, transparency give you the concise points,
+									summarizing the verbose and confusing terms that company
+									provide.
 								</p>
-								<div className='mt-4 items-center space-x-4'>
+								<div className='mt-4 ml-4 items-center space-y-3 sm:space-x-4 md:ml-0 md:mt-0'>
 									<button className='rounded-full border-[0.2rem] border-violet-500 px-5 py-2 shadow-lg'>
 										Discover app
 									</button>
-									<button className='rounded-full bg-gradient-to-br from-purple-900 via-violet-600 px-6 py-2 shadow-lg'>
+									<button className='block rounded-full bg-gradient-to-br from-purple-900 via-violet-600 px-6 py-2 shadow-lg sm:inline'>
 										Join our community
 									</button>
 								</div>
 							</div>
 
-							<div className='sbg-green-300/[0.5] col-span-5 justify-self-center'>
-								<TNC className='w-96 ' />
+							<div className='sbg-green-300/[0.5] hidden justify-self-center md:col-span-5 md:inline'>
+								<TNC className='w-96' />
 							</div>
 						</div>
 					</div>

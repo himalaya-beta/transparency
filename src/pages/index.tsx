6b2b636475fa52d0abbcef31ca2ff8c78d5850da
riolly <alberto.riolly@gmail.com>
2022-09-21 12:52:26 +0700

    :lipstick: fix: grid responsive layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
 				<section className='relative h-screen bg-gradient-to-br from-gray-900  via-purple-900 to-violet-600'>
 					<div className='absolute top-1/2 -translate-y-[50%] md:container md:left-1/2 md:max-w-screen-xl md:-translate-x-1/2 md:-translate-y-[60%]'>
 						<div className='mx-4 grid grid-cols-12 overflow-hidden pb-8 md:mx-0'>
-							<div className='animate__animated animate__fadeInUp  relative col-span-12 flex flex-col gap-2 self-center text-white md:col-span-7 md:ml-16 md:gap-4 lg:px-16'>
+							<div className='animate__animated animate__fadeInUp relative col-span-12 flex flex-col gap-2 self-center  text-white md:col-span-7 md:ml-16 md:min-w-[464px] md:gap-4 lg:min-w-[596px] lg:gap-6 lg:px-16'>
 								<h1 className='text-3xl md:text-5xl'>
 									Term and condition <br />
 									made&nbsp;
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
 									.&nbsp;Read <mark>&nbsp;highlights&nbsp;</mark>.&nbsp;
 									<span className='underline decoration-2'>Decide</span>!
 								</h2>
-								<p className='animate__animated animate__fadeInLeft animate__delay-2s ml-4 text-sm sm:mx-4 sm:text-justify sm:text-base md:-mt-2 md:ml-0 md:text-left'>
+								<p className='animate__animated animate__fadeInLeft animate__delay-2s ml-4 text-sm sm:mx-4 sm:text-justify sm:text-base md:ml-0 md:text-left lg:-mt-2'>
 									<span className='hidden sm:inline'>
 										No more getting abused by irresponsible companies who force
 										us to agree on something we don&apos;t want.&nbsp;
@@ -62,8 +62,8 @@ const Home: NextPage = () => {
 								</div>
 							</div>
 
-							<div className='animate__animated animate__fadeInRight hidden justify-self-center [animation-delay:300ms] md:col-span-5 md:inline'>
-								<TNC className='w-96' />
+							<div className='animate__animated animate__fadeInRight hidden justify-self-center  [animation-delay:300ms] md:col-span-5	md:-mr-[75%] md:inline lg:-mr-[25%] xl:mr-0'>
+								<TNC className='w-[28rem] max-w-full' />
 							</div>
 						</div>
 					</div>

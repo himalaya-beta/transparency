179a2bda08e51153f7e896757b7aa634e43859e1
riolly <alberto.riolly@gmail.com>
2022-09-21 08:11:55 +0700

    :lipstick: style: button hover & icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import type {NextPage} from 'next'
 import Head from 'next/head'
 
 import TNC from '../components/svg/tnc'
+import MagnifyingGlass from '/public/images/magnifying-glass.svg'
+import PeopleGroup from '/public/images/people-group.svg'
 
 import 'animate.css'
 
@@ -17,7 +19,7 @@ const Home: NextPage = () => {
 			<main className=''>
 				<section className='relative h-screen bg-gradient-to-br from-gray-900  via-purple-900 to-violet-600'>
 					<div className='sbg-red-300/[0.5] absolute top-1/2 -translate-y-[50%] md:container md:left-1/2 md:max-w-screen-xl md:-translate-x-1/2 md:-translate-y-[60%]'>
-						<div className='mx-4 grid grid-cols-12 overflow-hidden md:mx-0'>
+						<div className='sbg-red-200 mx-4 grid grid-cols-12 overflow-hidden pb-8 md:mx-0'>
 							<div className='sbg-blue-300/[0.5] animate__animated animate__fadeInUp  relative col-span-12 flex flex-col gap-2 self-center text-white md:col-span-7 md:ml-16 md:gap-4 lg:px-16'>
 								<h1 className='text-3xl md:text-5xl'>
 									Term and condition <br />
@@ -40,12 +42,14 @@ const Home: NextPage = () => {
 									summarizing the verbose and confusing terms that company
 									provide.
 								</p>
-								<div className='mt-4 ml-4 h-fit items-center space-y-3 pb-4 sm:space-x-4 md:ml-0 md:mt-0'>
-									<button className='rounded-full border-[0.2rem] border-purple-500 px-5 py-2 shadow-lg transition ease-out hover:border-violet-200 hover:text-purple-300 focus:border-violet-500 focus:outline-none focus:ring-2 focus:ring-white active:translate-y-2 active:text-white'>
+								<div className='mt-4 flex flex-col items-start gap-4 overflow-visible px-8 sm:flex-row sm:px-4 md:px-0'>
+									<button className='flex items-center gap-2 rounded-full border-[0.2rem] border-purple-500 px-5 py-2 shadow-lg transition duration-500 ease-out hover:border-violet-200 hover:shadow-purple-200 focus:border-violet-500 focus:outline-none focus:ring-2 focus:ring-white active:translate-y-2 active:text-white'>
 										Discover app
+										<MagnifyingGlass className='h-5 fill-violet-200' />
 									</button>
-									<button className='block rounded-full bg-violet-500 px-6 py-2 shadow-lg transition ease-out hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 active:translate-y-2  active:bg-violet-900 sm:inline'>
+									<button className='flex items-center gap-2 rounded-full bg-violet-500 px-6 py-2.5 shadow-lg transition duration-500 ease-out  hover:bg-violet-700 hover:shadow-purple-200 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 active:translate-y-2  active:bg-violet-900 sm:flex'>
 										Join our community
+										<PeopleGroup className='h-5 fill-purple-200' />
 									</button>
 								</div>
 							</div>

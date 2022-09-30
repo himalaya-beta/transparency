7caf02068e57cd384e21e571213e5a2a2d48ce00
riolly <alberto.riolly@gmail.com>
2022-09-30 15:10:31 +0700

    :lipstick: chore: translate-z 3d perspective

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const plugin = require('tailwindcss/plugin')
+// import plugin from 'tailwindcss/plugin'
+
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
@@ -20,9 +25,36 @@ module.exports = {
 		},
 	},
 	plugins: [
-		function ({addVariant}) {
+		plugin(function ({addUtilities, addVariant, matchUtilities, theme}) {
 			addVariant('child', '& > *')
 			addVariant('child-hover', '& > *:hover')
-		},
+
+			addUtilities({
+				'.preserve-3d': {
+					'transform-style': 'preserve-3d',
+				},
+				'.move-back': {
+					transform: 'translate3d(-1rem,-0.5rem,-2rem)',
+				},
+				'.move-forth': {
+					transform: 'translate3d(1rem,0.5rem,2rem)',
+				},
+				'.move-back-lg': {
+					transform: 'translate3d(-2rem,-1rem,-4rem)',
+				},
+				'.move-forth-lg': {
+					transform: 'translate3d(2rem,1rem,4rem)',
+				},
+			})
+
+			matchUtilities(
+				{
+					'translate-z': (value) => ({
+						transform: `translateZ(${value})`,
+					}),
+				},
+				{values: theme('translate')}
+			)
+		}),
 	],
 }

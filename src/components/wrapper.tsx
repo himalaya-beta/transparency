b99eddf75614bf2cc5d2a5d1845301ceef85f1db
riolly <alberto.riolly@gmail.com>
2022-11-19 22:39:57 +0700

    :recycle: refactor: QueryWrapper accept all props

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -1,23 +1,58 @@
 import React from 'react'
+import {type DefaultErrorShape} from '@trpc/server'
+import {type DefaultErrorData} from '@trpc/server/dist/error/formatter'
+
+type ErrorType = {
+	message: string
+	data?: DefaultErrorData | null
+	shape?: DefaultErrorShape | null | null
+}
 
 type Props<T> = {
 	isLoading: boolean
 	isError: boolean
-	data: T | undefined
+	data?: T
+	error: ErrorType | null
 	children: (entry: T) => JSX.Element
 }
 
-const QueryWrapper = <T,>({isLoading, isError, data, children}: Props<T>) => {
-	if (isLoading) return <p>Loading ...</p>
-	else if (isError) return <p>Error</p>
-	else if (data) {
+const QueryWrapper = <T,>({
+	isLoading,
+	isError,
+	data,
+	error,
+	children,
+}: Props<T>) => {
+	if (isLoading) {
+		return <Loading />
+	} else if (isError) {
+		return <Error error={error} />
+	} else if (data) {
 		return (Array.isArray(data) && data.length === 0) || data === null ? (
-			<p>Empty</p>
+			<Empty />
 		) : (
 			children(data)
 		)
 	}
 	return <React.Fragment />
 }
+
+const Loading = () => (
+	<div>
+		<p>Loading...</p>
+	</div>
+)
+
+const Error = ({error}: {error: ErrorType | null}) => (
+	<div>
+		<p>Error: {error?.message}</p>
+	</div>
+)
+
+const Empty = () => (
+	<div>
+		<p>There is not data</p>
+	</div>
+)
 
 export default QueryWrapper

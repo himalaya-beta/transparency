49ef651ec7bfbf26a4a97c64f8b57c32c22c0ec8
riolly <alberto.riolly@gmail.com>
2022-11-19 08:50:31 +0700

    :arrow_up: chore: update to t3 v6.10

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,9 @@
 // src/server/trpc/router/_app.ts
 import {router} from '../trpc'
 import {authRouter} from './auth'
-import {exampleRouter} from './example'
 
 export const appRouter = router({
 	auth: authRouter,
-	example: exampleRouter,
 })
 
 // export type definition of API

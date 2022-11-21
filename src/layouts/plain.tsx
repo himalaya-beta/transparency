0094506b3c90c04bcb6268a7254140b6249f1e08
riolly <alberto.riolly@gmail.com>
2022-11-21 12:48:34 +0700

    :sparkles: feat: nav on layout

--- src/layouts/plain.tsx
+++ src/layouts/plain.tsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import {useRouter} from 'next/router'
+import Link from 'next/link'
+
+import {capFirstChar} from '@utils/literal'
+import {MdArticle, MdHome} from 'react-icons/md'
 
 export default function PlainLayout({children}: {children: React.ReactNode}) {
+	const {pathname} = useRouter()
+	const routes = [
+		{href: '/', name: 'home', icon: <MdHome />},
+		{href: '/article', name: 'article', icon: <MdArticle />},
+	]
+
 	return (
-		<div className='min-h-screen bg-gradient-to-br from-purple-900 to-gray-900 py-12'>
-			<main className='container mx-auto'>{children}</main>
+		<div className='min-h-screen bg-gradient-to-br from-purple-900 to-gray-900 '>
+			<div className='glass flex h-12 w-full items-center justify-center border-none  bg-opacity-30 shadow-sm'>
+				<nav className='flex h-fit gap-4'>
+					{routes.map(({href, name, icon}) => {
+						const isActive = pathname === href
+						return (
+							<>
+								<Link
+									href={href}
+									key={href}
+									className={`flex items-center gap-2 px-4 font-medium text-gray-50 ${
+										isActive
+											? 'pointer-events-none text-violet-300'
+											: 'hover:underline'
+									}`}
+								>
+									<span className='text-xl'>{icon}</span>
+									<span>{capFirstChar(name)}</span>
+								</Link>
+							</>
+						)
+					})}
+				</nav>
+			</div>
+			<main className='container mx-auto py-12'>{children}</main>
 		</div>
 	)
 }

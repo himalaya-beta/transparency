ffdf3291522d7d49c3d8e67437c7834b89cdef35
riolly <alberto.riolly@gmail.com>
2022-11-22 22:26:20 +0700

    :recycle: refactor: create article form

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -5,9 +5,22 @@ import {trpc} from '@utils/trpc'
 import PlainLayout from 'layouts/plain'
 import GlassContainerLayout from 'layouts/glass-container'
 import QueryWrapper from '@components/query-wrapper'
-import CreateArticleForm from '@components/article-form'
 
 import {ArticleType} from '@type/article'
+
+import {
+	useForm,
+	type SubmitHandler,
+	type SubmitErrorHandler,
+} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+
+import FormWrapper from '@components/form'
+import TextAreaInput from '@components/textarea-input'
+import Button from '@components/button'
+import {MdCreate as Create} from 'react-icons/md'
+
+import {CreateArticleSchema, type CreateArticleType} from '@type/article'
 
 export default function ArticlePage() {
 	const articlesQuery = trpc.article.fetchAll.useQuery()
@@ -38,6 +51,50 @@ const Card = ({slug, title, content}: ArticleType) => {
 			<h2 className='text-xl text-gray-50'>{title}</h2>
 			<p className='text-gray-200 line-clamp-6'>{content}</p>
 		</Link>
+	)
+}
+const onInvalid: SubmitErrorHandler<CreateArticleType> = (data) => {
+	console.log(data)
+}
+
+const CreateArticleForm = () => {
+	const methods = useForm<CreateArticleType>({
+		mode: 'onTouched',
+		resolver: zodResolver(CreateArticleSchema),
+	})
+
+	const {mutate} = trpc.article.create.useMutation({
+		onError: (error) => {
+			alert(JSON.stringify(error.message))
+		},
+		onSuccess: () => {
+			methods.reset()
+		},
+	})
+
+	const onValid: SubmitHandler<CreateArticleType> = (data) => {
+		mutate(data)
+	}
+
+	return (
+		<div className='grid grid-cols-4'>
+			<FormWrapper
+				methods={methods}
+				onValid={onValid}
+				onInvalid={onInvalid}
+				className='col-span-full flex flex-col gap-4 md:col-span-2'
+			>
+				<TextAreaInput name='title' />
+				<TextAreaInput name='content' rows={5} />
+				<Button
+					type='submit'
+					variant='outlined'
+					className='w-fit text-gray-200'
+				>
+					Create <Create className='text-lg text-white' />
+				</Button>
+			</FormWrapper>
+		</div>
 	)
 }
 

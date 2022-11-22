2e581c115db60836f3f51fab073d2ac0df558a59
riolly <alberto.riolly@gmail.com>
2022-11-22 15:31:58 +0700

    :recycle: chore: format response

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -2,23 +2,32 @@ import {NextApiRequest, NextApiResponse} from 'next'
 
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse
+	res: NextApiResponse<{
+		revalidated: boolean
+		path: string
+		message?: string
+		error?: unknown
+	}>
 ) {
+	const body = JSON.parse(req.body)
+	const defaultRes = {revalidated: false, path: body.path}
+
 	if (req.method !== 'POST') {
-		return res.status(400).json({message: 'Invalid HTTP method'})
+		return res.status(400).json({...defaultRes, message: 'Invalid HTTP method'})
 	}
 
-	const body = JSON.parse(req.body)
 	if (!body.path) {
-		return res.status(400).json({message: 'Path required'})
+		return res.status(400).json({...defaultRes, message: 'Path required'})
 	}
 
 	try {
 		await res.revalidate(body.path)
-		console.log('[REVALIDATING]', body.path)
-		return res.status(200).json({revalidated: true})
+		console.log('[REVALIDATE]', body.path)
+		return res.status(200).json({...defaultRes, revalidated: true})
 	} catch (error) {
-		console.log(error)
-		return res.status(500).send('Error revalidating')
+		console.log('[REVALIDATE]', error)
+		return res
+			.status(500)
+			.json({...defaultRes, message: 'Invalidation error', error})
 	}
 }

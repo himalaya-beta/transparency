cc0c9977ca685f18c959e170340e166fe3a39fb4
riolly <alberto.riolly@gmail.com>
2022-12-09 18:03:01 +0700

    :lipstick: style: section separator

---
+++ src/components/ornaments.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const SectionSeparator = ({children}: {children: string}) => {
+	return (
+		<div className='flex items-center justify-center gap-4  text-light-head'>
+			<div className='h-[1px] w-auto grow rounded-full bg-brand-500/50' />
+			<TriangleSymbol />
+			<p className='w-fit text-lg'>{children}</p>
+			<TriangleSymbol className='rotate-180' />
+			<div className='h-[1px] w-auto grow rounded-full bg-brand-500/50' />
+		</div>
+	)
+}
+
+export const TriangleSymbol = ({className}: {className?: string}) => {
+	return <span className={`${className} text-brand-300`}>⨞</span>
+}

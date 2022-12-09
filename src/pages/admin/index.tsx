23dce846eb5097d93197fb9f4c7d2d93499ad781
riolly <alberto.riolly@gmail.com>
2022-12-10 06:01:12 +0700

    :construction: wip: create sub criteria

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -9,7 +9,12 @@ import FormWrapper from 'components/form-wrapper'
 import TextAreaInput from 'components/textarea-input'
 import {Button} from 'components/button'
 import {SectionSeparator} from 'components/ornaments'
-import {PencilIcon, TrashIcon, XMarkIcon} from '@heroicons/react/24/outline'
+import {
+	PencilIcon,
+	PlusIcon,
+	TrashIcon,
+	XMarkIcon,
+} from '@heroicons/react/24/outline'
 
 import {trpc} from 'utils/trpc'
 
@@ -18,22 +23,33 @@ import {
 	criteriaUpdateSchema,
 	type CriteriaUpdateType,
 	type CriteriaCreateType,
+	CriteriaType,
 } from 'types/criteria'
 
 const AdminDashboardPage = () => {
 	const [edit, setEdit] = React.useState<string | null>(null)
+	const [add, setAdd] = React.useState<string | null>(null)
 
 	const criteriaListQuery = trpc.criteria.fetchAll.useQuery()
-	const {refetch: refetchCriteria} = criteriaListQuery
 
+	const {mutate: create} = trpc.criteria.create.useMutation({
+		onError: (error) => {
+			alert(error.message)
+		},
+		onSuccess: () => {
+			createMethods.reset()
+			criteriaListQuery.refetch()
+			setAdd(null)
+		},
+	})
 	const {mutate: update} = trpc.criteria.update.useMutation({
 		onError: (error) => {
 			alert(error.message)
 		},
 		onSuccess: () => {
 			updateMethods.reset()
-			setEdit(null)
 			criteriaListQuery.refetch()
+			setEdit(null)
 		},
 	})
 	const {mutate: remove} = trpc.criteria.delete.useMutation({
@@ -45,22 +61,40 @@ const AdminDashboardPage = () => {
 		},
 	})
 
+	const createMethods = useForm<CriteriaCreateType>({
+		mode: 'onTouched',
+		resolver: zodResolver(criteriaCreateSchema),
+	})
+	const createSubMethods = useForm<CriteriaCreateType>({
+		mode: 'onTouched',
+		resolver: zodResolver(criteriaCreateSchema),
+	})
 	const updateMethods = useForm<CriteriaUpdateType>({
 		mode: 'onTouched',
 		resolver: zodResolver(criteriaUpdateSchema),
 	})
 
+	const onCreateCriteria: SubmitHandler<CriteriaCreateType> = (data) => {
+		create(data)
+	}
 	const onUpdateCriteria: SubmitHandler<CriteriaUpdateType> = (data) => {
 		if (edit) {
 			update({...data, id: edit})
 		}
 	}
 
-	const onClickEdit = (criteria: CriteriaUpdateType) => {
+	const onClickEdit = (criteria: CriteriaType) => {
 		setEdit(criteria.id)
+		setAdd(null)
 		updateMethods.setValue('id', criteria.id)
 		updateMethods.setValue('value', criteria.value)
 		updateMethods.setValue('parentId', criteria.parentId)
+	}
+
+	const onClickAddSubCriteria = (criteria: CriteriaType) => {
+		setAdd(criteria.id)
+		setEdit(null)
+		createSubMethods.setValue('parentId', criteria.id)
 	}
 
 	return (
@@ -72,7 +106,7 @@ const AdminDashboardPage = () => {
 						{criterias.map((criteria) => (
 							<div
 								key={criteria.id}
-								className='flex items-center justify-between rounded-lg bg-dark-bg/25 p-4'
+								className='space-y-4 rounded-lg bg-dark-bg/25 p-4'
 							>
 								{edit === criteria.id ? (
 									<FormWrapper
@@ -98,15 +132,43 @@ const AdminDashboardPage = () => {
 									</FormWrapper>
 								) : (
 									<>
-										<h2>{criteria.value}</h2>
-										<div className='item-center flex gap-2'>
-											<button onClick={() => onClickEdit(criteria)}>
-												<PencilIcon className='h-10 w-10 rounded-lg bg-brand-100 p-2 text-blue-500' />
-											</button>
-											<button onClick={() => remove({id: criteria.id})}>
-												<TrashIcon className='h-10 w-10 rounded-lg bg-brand-100 p-2 text-red-500' />
-											</button>
+										<div className='flex items-center justify-between'>
+											<h2>{criteria.value}</h2>
+											<div className='item-center flex gap-2'>
+												<button onClick={() => onClickAddSubCriteria(criteria)}>
+													<PlusIcon className='h-10 w-10 rounded-lg bg-brand-100 p-2 text-blue-500' />
+												</button>
+												<button onClick={() => onClickEdit(criteria)}>
+													<PencilIcon className='h-10 w-10 rounded-lg bg-brand-100 p-2 text-blue-500' />
+												</button>
+												<button onClick={() => remove({id: criteria.id})}>
+													<TrashIcon className='h-10 w-10 rounded-lg bg-brand-100 p-2 text-red-500' />
+												</button>
+											</div>
 										</div>
+										{add === criteria.id && (
+											<FormWrapper
+												methods={createSubMethods}
+												onValidSubmit={onCreateCriteria}
+												className='flex w-full gap-2'
+											>
+												<TextAreaInput<CriteriaUpdateType>
+													name='value'
+													rows={2}
+													label=''
+													wrapperClassName='flex-1'
+													inputClassName='pb-3'
+												/>
+												<div className='flex flex-col gap-1'>
+													<button type='submit'>
+														<PencilIcon className='h-8 w-8 rounded-lg bg-blue-500 p-1 text-brand-100' />
+													</button>
+													<button onClick={() => setEdit(null)}>
+														<XMarkIcon className='h-8 w-8 rounded-lg bg-orange-400 p-1 text-brand-100' />
+													</button>
+												</div>
+											</FormWrapper>
+										)}
 									</>
 								)}
 							</div>
@@ -115,44 +177,22 @@ const AdminDashboardPage = () => {
 				)}
 			</QueryWrapper>
 
-			<CreateForm refetchList={refetchCriteria} />
-		</div>
-	)
-}
-
-const CreateForm = ({refetchList}: {refetchList: () => void}) => {
-	const {mutate: create} = trpc.criteria.create.useMutation({
-		onError: (error) => {
-			alert(error.message)
-		},
-		onSuccess: () => {
-			createMethods.reset()
-			refetchList()
-		},
-	})
-
-	const createMethods = useForm<CriteriaCreateType>({
-		mode: 'onTouched',
-		resolver: zodResolver(criteriaCreateSchema),
-	})
-
-	const onCreateCriteria: SubmitHandler<CriteriaCreateType> = (data) => {
-		create(data)
-	}
-
-	return (
-		<div>
-			<SectionSeparator>Create New</SectionSeparator>
-			<FormWrapper
-				methods={createMethods}
-				onValidSubmit={onCreateCriteria}
-				className='space-y-4'
-			>
-				<TextAreaInput<CriteriaCreateType> name='value' label='The point' />
-				<Button type='submit' variant='filled'>
-					Submit
-				</Button>
-			</FormWrapper>
+			<div>
+				<SectionSeparator>Create New</SectionSeparator>
+				<FormWrapper
+					methods={createMethods}
+					onValidSubmit={onCreateCriteria}
+					className='space-y-4'
+				>
+					<TextAreaInput<CriteriaCreateType>
+						name='value'
+						label='Criteria name/ content/ value'
+					/>
+					<Button type='submit' variant='filled'>
+						Submit
+					</Button>
+				</FormWrapper>
+			</div>
 		</div>
 	)
 }

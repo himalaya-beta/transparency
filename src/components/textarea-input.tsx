950c58cfd3b19a462212790af636343972799aa2
riolly <alberto.riolly@gmail.com>
2022-12-09 17:47:58 +0700

    :recycle: refactor: className for each tag

--- src/components/textarea-input.tsx
+++ src/components/textarea-input.tsx
@@ -5,12 +5,19 @@ import {capFirstChar} from 'utils/literal'
 type InputProps<T> = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
 	name: keyof T
 	label?: string
+	wrapperClassName?: string
+	labelClassName?: string
+	inputClassName?: string
+	errorClassName?: string
 }
 
 const TextAreaInput = <T,>({
 	name,
 	label,
-	className,
+	wrapperClassName,
+	labelClassName,
+	inputClassName,
+	errorClassName,
 	...props
 }: InputProps<T>) => {
 	const {
@@ -18,13 +25,13 @@ const TextAreaInput = <T,>({
 		formState: {errors},
 	} = useFormContext()
 	return (
-		<div className='flex flex-col'>
-			<label htmlFor={name} className='text-light-head'>
+		<div className={`flex flex-col ${wrapperClassName}`}>
+			<label htmlFor={name} className={`text-light-head ${labelClassName}`}>
 				{label ?? capFirstChar(name)}
 			</label>
 			<textarea
 				id={name}
-				className={`rounded bg-light-bg/80 py-2 px-4 ${className}`}
+				className={`rounded bg-light-bg/80 py-2 px-4 ${inputClassName}`}
 				{...register(name)}
 				{...props}
 			/>
@@ -32,7 +39,11 @@ const TextAreaInput = <T,>({
 				name={name}
 				errors={errors}
 				render={({message}) => (
-					<small className='mt-0.5 font-medium text-red-500'>{message}</small>
+					<small
+						className={`mt-0.5 font-medium text-red-500 ${errorClassName}`}
+					>
+						{message}
+					</small>
 				)}
 			/>
 		</div>

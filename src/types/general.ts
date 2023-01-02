68f10c67a24b9cc0b91ab9cc8d64af9ee75d8add
riolly <alberto.riolly@gmail.com>
2023-01-02 15:22:28 +0700

    :label: chore(type): infer array element

--- src/types/general.ts
+++ src/types/general.ts
@@ -1,3 +1,6 @@
 import {z} from 'zod'
 
 export const requiredIdSchema = z.object({id: z.string()})
+
+export type ArrayElement<ArrayType extends readonly unknown[]> =
+	ArrayType extends readonly (infer ElementType)[] ? ElementType : never

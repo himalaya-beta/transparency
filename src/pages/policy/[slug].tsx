68ee4a846062bcc6c1dbb49741a057822ac491ea
riolly <alberto.riolly@gmail.com>
2023-01-02 13:15:27 +0700

    :construction: wip: criteria list

--- src/pages/policy/[slug].tsx
+++ src/pages/policy/[slug].tsx
@@ -1,17 +1,25 @@
+/* eslint-disable unicorn/no-null */
 import React from 'react'
+import {useAutoAnimate} from '@formkit/auto-animate/react'
+
+import {prisma} from 'server/db/client'
+import {trpc} from 'utils/trpc'
+import {extractIdFromSlug, slugify} from 'utils/literal'
+
+import NavbarLayout from 'layouts/navbar'
+import MetaHead from 'components/meta-head'
+import QueryWrapper from 'components/query-wrapper'
+import DivAnimate from 'components/div-animate'
+import {TriangleSymbol} from 'components/ornaments'
+import {CheckIcon} from '@heroicons/react/24/outline'
+
 import {
 	type GetStaticPaths,
 	type GetStaticProps,
 	type InferGetStaticPropsType,
 } from 'next'
-
-import {prisma} from 'server/db/client'
-import {extractIdFromSlug, slugify} from 'utils/literal'
-
-import {useAutoAnimate} from '@formkit/auto-animate/react'
-import NavbarLayout from 'layouts/navbar'
-import MetaHead from 'components/meta-head'
-import {AppType} from 'types/app'
+import {type AppType} from 'types/app'
+import {type CriteriaType} from 'types/criteria'
 
 export const getStaticProps: GetStaticProps<{
 	app: AppType
@@ -22,7 +30,7 @@ export const getStaticProps: GetStaticProps<{
 
 	const app = await prisma.app.findUnique({
 		where: {id},
-		include: {AppCriteria: true},
+		include: {AppCriteria: {include: {criteria: true}}},
 	})
 
 	if (!app) return {notFound: true}
@@ -45,10 +53,20 @@ export const getStaticPaths: GetStaticPaths = async () => {
 	}
 }
 
+type CriteriaFilledType = CriteriaType & {
+	checked: boolean
+	explanation: string | null
+}
+
 const AppDetailsPage = ({
 	app,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
 	const [toggleAnimation] = useAutoAnimate<HTMLDivElement>()
+
+	const criteriaQ = trpc.criteria.fetchByApp.useQuery(
+		{appId: app.id},
+		{refetchOnWindowFocus: false}
+	)
 
 	return (
 		<>
@@ -58,13 +76,81 @@ const AppDetailsPage = ({
 				imageUrl={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/images/articles.jpg`}
 			/>
 			<main
-				className='container mx-auto max-w-screen-md space-y-8 px-6 pt-6'
+				className='container mx-auto max-w-screen-lg space-y-8 px-6 pt-6'
 				ref={toggleAnimation}
 			>
-				<h1 className='text-3xl'>{app.name}</h1>
-				<p className='text-lg '>{app.about}</p>
+				<div className='max-w-screen-md'>
+					<h1 className='text-3xl'>{app.name}</h1>
+					<p className='italic text-opacity-75'>{app.company}</p>
+					<p className='mt-4 text-lg'>{app.about}</p>
+				</div>
+
+				<QueryWrapper {...criteriaQ}>
+					{(data) => (
+						<ul className='space-y-4'>
+							{data.map((criteria) => (
+								<CriteriaList key={criteria.id} criteria={criteria} />
+							))}
+						</ul>
+					)}
+				</QueryWrapper>
 			</main>
 		</>
+	)
+}
+
+const CriteriaList = ({criteria}: {criteria: CriteriaFilledType}) => {
+	const hasChildren = criteria.children.length > 0
+	const [isExpanded, setIsExpanded] = React.useState(criteria.checked)
+
+	const onExpand = () => {
+		if (hasChildren) {
+			setIsExpanded(!isExpanded)
+		}
+	}
+
+	return (
+		<li className='flex list-none items-start gap-2'>
+			<TriangleSymbol
+				onClick={onExpand}
+				className={`
+					transition-transform hover:cursor-pointer
+					${hasChildren ? 'visible' : 'invisible'}
+					${isExpanded ? '-rotate-90' : '-rotate-180'}
+				`}
+			/>
+			<div className='w-full'>
+				<div className='grid grid-cols-8 border-b-[1px] border-brand-100/50'>
+					<h3
+						onClick={onExpand}
+						className={`
+							col-span-5 text-lg font-medium 
+							${hasChildren ? 'hover:cursor-pointer' : ''}
+						`}
+					>
+						{criteria.value}
+					</h3>
+
+					<div className='col-span-3 flex h-full w-fit justify-center justify-self-end'>
+						{criteria.type === 'TRUE_FALSE' && criteria.checked && (
+							<CheckIcon className='mr-8 w-6 text-brand-200' />
+						)}
+						{criteria.type === 'EXPLANATION' && (
+							<p className='text-right text-sm'>{criteria.explanation}</p>
+						)}
+					</div>
+				</div>
+
+				<DivAnimate className='pt-2'>
+					{isExpanded &&
+						criteria.children.map((children) => (
+							<div key={children.id} className='pl-4'>
+								<h3 className='text-md font-light'>{children.value}</h3>
+							</div>
+						))}
+				</DivAnimate>
+			</div>
+		</li>
 	)
 }
 

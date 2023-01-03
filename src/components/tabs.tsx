ede39db96989e5a106449807198ee56faf40198c
riolly <alberto.riolly@gmail.com>
2023-01-03 15:39:34 +0700

    :bug: fix: remove select on sm, change hover effect

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -10,45 +10,27 @@ export default function Tabs({
 	setTabActive: Dispatch<string>
 }) {
 	return (
-		<div>
-			<div className='sm:hidden'>
-				<label htmlFor='tabs' className='sr-only'>
-					Select a tab
-				</label>
-				<select
-					id='tabs'
-					name='tabs'
-					className='block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-brand-500 focus:outline-none focus:ring-brand-500 sm:text-sm'
-					defaultValue={tabActive}
-					onChange={(e) => setTabActive(e.target.value)}
-				>
+		<div className='border-t-2 border-gray-200/75 md:border-none md:px-8'>
+			<div className='border-t-[1px] border-gray-200/75'>
+				<nav className='-mb-px flex flex-row-reverse' aria-label='Tabs'>
 					{tabs.map((tab) => (
-						<option key={'button_' + tab}>{tab}</option>
-					))}
-				</select>
-			</div>
-			<div className='hidden sm:block'>
-				<div className='border-t border-gray-200/75'>
-					<nav className='-mb-px flex flex-row-reverse' aria-label='Tabs'>
-						{tabs.map((tab) => (
-							<button
-								key={'button_' + tab}
-								className={`
+						<button
+							key={'button_' + tab}
+							className={`
                   ${
 										tabActive === tab
-											? 'border-gray-200/75 font-bold '
-											: 'border-transparent'
+											? 'border-gray-200/75 font-heading font-extrabold'
+											: 'border-transparent font-normal'
 									}
-                  whitespace-nowrap border-t-2 px-4 py-2 text-sm font-medium hover:border-brand-300 hover:text-brand-300
+                  whitespace-nowrap border-t-2 px-4 py-2 text-sm hover:font-semibold
                 `}
-								onClick={() => setTabActive(tab)}
-								aria-current={tabActive === tab ? 'page' : undefined}
-							>
-								{tab}
-							</button>
-						))}
-					</nav>
-				</div>
+							onClick={() => setTabActive(tab)}
+							aria-current={tabActive === tab ? 'page' : undefined}
+						>
+							{tab}
+						</button>
+					))}
+				</nav>
 			</div>
 		</div>
 	)

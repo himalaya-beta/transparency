7d2dfd0c795495bd324c76580187108f3b4857f5
riolly <alberto.riolly@gmail.com>
2023-01-15 14:51:52 +0700

    :lipstick: style: darken modal bg

--- src/pages/policy/index.tsx
+++ src/pages/policy/index.tsx
@@ -530,7 +530,7 @@ function Modal({
 					leaveFrom='opacity-100'
 					leaveTo='opacity-0'
 				>
-					<div className='fixed inset-0 bg-black bg-opacity-25' />
+					<div className='fixed inset-0 bg-black bg-opacity-50' />
 				</Transition.Child>
 
 				<div className='fixed inset-0 overflow-y-auto'>

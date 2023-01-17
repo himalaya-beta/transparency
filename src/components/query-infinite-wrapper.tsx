13b5d52f59f282a7277edab783c55714e2ba7eda
riolly <alberto.riolly@gmail.com>
2023-01-17 22:35:46 +0700

    :building_construction: change: jotai w/ zustand & immer

--- src/components/query-infinite-wrapper.tsx
+++ src/components/query-infinite-wrapper.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {atom, useAtom, useAtomValue} from 'jotai'
-import {focusAtom} from 'jotai-optics'
+import {create} from 'zustand'
+import {shallow} from 'zustand/shallow'
+import {produce} from 'immer'
 import {useAutoAnimate} from '@formkit/auto-animate/react'
 
 import {
@@ -16,22 +17,79 @@ import {
 	type InfiniteData,
 } from '@tanstack/react-query'
 
+const isArrayIdentic = (a: string[], b: string[]) => {
+	if (a.length === b.length) {
+		for (const [i, element] of a.entries()) {
+			if (element !== b[i]) {
+				return false
+			}
+		}
+		return true
+	} else {
+		return false
+	}
+}
+
 type PaginationObject = {
+	keys: Array<string>
 	pages: Array<number>
 	pageActive: number
 }
 const initialParams = {
+	keys: [],
 	pages: [1],
 	pageActive: 1,
 }
-const initialValues = {
-	policy: initialParams,
-	community: initialParams,
+const pagesAvailable = ['policy', 'community'] as const
+type PagesAvailable = typeof pagesAvailable[number]
+
+type initialValues = Record<PagesAvailable, Array<PaginationObject>> & {
+	addPage: (name: PagesAvailable, keys: Array<string>) => void
+	setPageActive: (
+		name: PagesAvailable,
+		keys: Array<string>,
+		pageActive: number
+	) => void
 }
-const paginationAtom = atom<Record<string, PaginationObject>>(initialValues)
+
+const usePagination = create<initialValues>()((set) => ({
+	policy: [initialParams],
+	community: [initialParams],
+	addPage: (name, keys) =>
+		set((state) => {
+			const idx = state[name].findIndex((item) =>
+				isArrayIdentic(item.keys, keys)
+			)
+
+			const updated = produce(state[name], (draft) => {
+				const pages = draft[idx]?.pages
+				if (pages) {
+					pages.push(pages.length + 1)
+				}
+			})
+
+			return {[name]: updated}
+		}),
+	setPageActive: (name, keys, pageActive) =>
+		set((state) => {
+			const idx = state[name].findIndex((item) =>
+				isArrayIdentic(item.keys, keys)
+			)
+
+			const updated = produce(state[name], (draft) => {
+				const current = draft[idx]
+				if (current) {
+					current.pageActive = pageActive
+				}
+			})
+
+			return {[name]: updated}
+		}),
+}))
 
 type Props<T> = {
-	name: keyof typeof initialValues
+	name: PagesAvailable
+	keys?: Array<string>
 	hasNextPage: boolean | undefined
 	fetchNextPage: (
 		options?: FetchNextPageOptions
@@ -48,6 +106,7 @@ type Props<T> = {
 
 const DataInfiniteWrapper = <T,>({
 	name,
+	keys = [],
 	hasNextPage,
 	fetchNextPage,
 	data,
@@ -56,34 +115,33 @@ const DataInfiniteWrapper = <T,>({
 }: Props<T>) => {
 	const device = useDeviceDetect()
 
-	// -------------------------------- ATOMS -------------------------------- //
-	const currentPaginationAtom = React.useMemo(
-		() => focusAtom(paginationAtom, (optic) => optic.prop(name)),
-		[name]
-	)
-	const pagesAtom = React.useMemo(
-		() => focusAtom(currentPaginationAtom, (optic) => optic.prop('pages')),
-		[currentPaginationAtom]
-	)
-	const pageAtom = React.useMemo(
-		() => focusAtom(currentPaginationAtom, (optic) => optic.prop('pageActive')),
-		[currentPaginationAtom]
-	)
-	const lastPageAtom = React.useMemo(
-		() => atom((get) => get(currentPaginationAtom).pages.length),
-		[currentPaginationAtom]
-	)
-	const [pages, setPages] = useAtom(pagesAtom)
-	const [page, setPage] = useAtom(pageAtom)
-	const lastPage = useAtomValue(lastPageAtom)
+	// -------------------------------- STORE -------------------------------- //
+	const addPage = usePagination((state) => state.addPage)
+	const setPage = usePagination((state) => state.setPageActive)
+	const {pages, pageActive} = usePagination((state) => {
+		const current = state[name].find((item) => isArrayIdentic(item.keys, keys))
+		// TODO: update store if it's not exist
+		return {
+			pages: current?.pages ?? [1],
+			pageActive: current?.pageActive ?? 1,
+		}
+	}, shallow)
+	const pageLast = pages.length
+
+	const setPageActive = (page: number) => setPage(name, keys, page)
 
 	// ----------------------------- Prefetching ----------------------------- //
+	const refInit = React.useRef(false)
 	React.useEffect(() => {
-		if (hasNextPage && page === lastPage) {
-			fetchNextPage()
-			setPages([...pages, page + 1])
+		if (refInit.current) {
+			if (hasNextPage && pageActive === pageLast) {
+				fetchNextPage()
+				addPage(name, keys)
+			}
+		} else {
+			refInit.current = true
 		}
-	}, [hasNextPage, page, lastPage, fetchNextPage, setPages, pages])
+	}, [addPage, fetchNextPage, hasNextPage, keys, name, pageActive, pageLast])
 
 	// ------------------------------ Animation ------------------------------ //
 	const [refAnimate, enableAnimations] = useAutoAnimate<HTMLDivElement>()
@@ -99,18 +157,18 @@ const DataInfiniteWrapper = <T,>({
 		<div className='space-y-4 md:space-y-6'>
 			<div className={className} ref={refAnimate}>
 				{data?.pages.map(({items}, i) => {
-					if (i + 1 !== page && !device.isPhone) return
-					if (i + 1 !== page + 1 || page === lastPage)
+					if (i + 1 !== pageActive && !device.isPhone) return
+					if (i + 1 !== pageActive + 1 || pageActive === pageLast)
 						return items.map((item) => children(item))
 				})}
 			</div>
 
 			{device.isPhone ? (
 				<>
-					{page !== lastPage && (
+					{pageActive !== pageLast && (
 						<div className='flex justify-center'>
 							<button
-								onClick={() => setPage(page + 1)}
+								onClick={() => setPageActive(pageActive + 1)}
 								className='rounded-lg bg-white/20 px-4 py-2'
 							>
 								Load more ..
@@ -121,8 +179,8 @@ const DataInfiniteWrapper = <T,>({
 			) : (
 				<div className='mx-auto flex h-16 w-fit items-center gap-2 transition-all'>
 					<button
-						onClick={() => void setPage(page - 1)}
-						disabled={page === 1}
+						onClick={() => void setPageActive(pageActive - 1)}
+						disabled={pageActive === 1}
 						className='p-2 text-white transition-transform hover:scale-150 disabled:transform-none disabled:text-gray-500'
 					>
 						<ChevronDoubleLeftIcon className='h-4 w-4 ' />
@@ -132,13 +190,13 @@ const DataInfiniteWrapper = <T,>({
 						{pages.map((i) => {
 							const span = 3 + 1
 							const isFirst = i === 1
-							const isActive = i === page
+							const isActive = i === pageActive
 							const isLast = i === pages.length
 
 							const label = i
 							const key = `page_button_${i}`
 
-							const onClick = () => void setPage(i)
+							const onClick = () => void setPageActive(i)
 
 							return (
 								<React.Fragment key={key}>
@@ -146,11 +204,11 @@ const DataInfiniteWrapper = <T,>({
 										<>
 											<PageButton
 												key={`${key}_first`}
-												label={page >= 1 + span + 1 ? 'first' : label}
+												label={pageActive >= 1 + span + 1 ? 'first' : label}
 												isActive={isActive}
 												onClick={onClick}
 											/>
-											{page >= 1 + span + 1 && (
+											{pageActive >= 1 + span + 1 && (
 												<span
 													key={`${key}_dot_left`}
 													className='text-light-body'
@@ -161,7 +219,7 @@ const DataInfiniteWrapper = <T,>({
 										</>
 									)}
 
-									{i < page && i > page - span && !isFirst && (
+									{i < pageActive && i > pageActive - span && !isFirst && (
 										<PageButton
 											key={`${key}_left_${i}`}
 											label={label}
@@ -179,7 +237,7 @@ const DataInfiniteWrapper = <T,>({
 										/>
 									)}
 
-									{i > page && i < page + span && !isLast && (
+									{i > pageActive && i < pageActive + span && !isLast && (
 										<PageButton
 											key={`${key}_right_${i}`}
 											label={label}
@@ -190,7 +248,7 @@ const DataInfiniteWrapper = <T,>({
 
 									{isLast && !isActive && (
 										<>
-											{i > page + span && (
+											{i > pageActive + span && (
 												<span
 													key={`${key}_dot_left`}
 													className='text-light-body'
@@ -200,7 +258,7 @@ const DataInfiniteWrapper = <T,>({
 											)}
 											<PageButton
 												key={`${key}_last`}
-												label={i > page + span ? 'last' : label}
+												label={i > pageActive + span ? 'last' : label}
 												isActive={isActive}
 												onClick={onClick}
 											/>
@@ -212,8 +270,8 @@ const DataInfiniteWrapper = <T,>({
 					</div>
 
 					<button
-						onClick={() => void setPage(page + 1)}
-						disabled={!hasNextPage && page === lastPage}
+						onClick={() => void setPageActive(pageActive + 1)}
+						disabled={!hasNextPage && pageActive === pageLast}
 						className='p-2 text-white transition-transform hover:scale-125 disabled:transform-none disabled:text-gray-500'
 					>
 						<ChevronDoubleRightIcon className='h-4 w-4' />

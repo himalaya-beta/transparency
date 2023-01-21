f0b48772b0af5af0a84f607e49e8a3549c22e2e4
riolly <alberto.riolly@gmail.com>
2023-01-21 14:02:07 +0700

    :bento: add: rich text menu icons

---
+++ src/components/svg/align-center.tsx
@@ -0,0 +1,28 @@
+type Props = {
+	className?: string
+	primaryClassName?: string
+	secondaryClassName?: string
+}
+
+const AlignCenterIcon = ({
+	className = 'h-5 w-5',
+	primaryClassName,
+	secondaryClassName,
+}: Props) => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		viewBox='0 0 448 512'
+		className={className}
+	>
+		<path
+			className={primaryClassName}
+			d='M32 480C14.33 480 0 465.7 0 448C0 430.3 14.33 416 32 416H416C433.7 416 448 430.3 448 448C448 465.7 433.7 480 416 480H32zM32 224C14.33 224 0 209.7 0 192C0 174.3 14.33 160 32 160H416C433.7 160 448 174.3 448 192C448 209.7 433.7 224 416 224H32z'
+		/>
+		<path
+			className={secondaryClassName}
+			d='M128 352C110.3 352 96 337.7 96 320C96 302.3 110.3 288 128 288H320C337.7 288 352 302.3 352 320C352 337.7 337.7 352 320 352H128zM128 96C110.3 96 96 81.67 96 64C96 46.33 110.3 32 128 32H320C337.7 32 352 46.33 352 64C352 81.67 337.7 96 320 96H128z'
+		/>
+	</svg>
+)
+
+export default AlignCenterIcon
---
+++ src/components/svg/quote-left.tsx
@@ -0,0 +1,28 @@
+type Props = {
+	className?: string
+	primaryClassName?: string
+	secondaryClassName?: string
+}
+
+const QuoteLeftIcon = ({
+	className = 'h-5 w-5',
+	primaryClassName,
+	secondaryClassName,
+}: Props) => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		viewBox='0 0 448 512'
+		className={className}
+	>
+		<path
+			className={primaryClassName}
+			d='M96 224C84.72 224 74.05 226.3 64 229.9V224c0-35.3 28.7-64 64-64c17.67 0 32-14.33 32-32S145.7 96 128 96C57.42 96 0 153.4 0 224v96c0 53.02 42.98 96 96 96s96-42.98 96-96S149 224 96 224z'
+		/>
+		<path
+			className={secondaryClassName}
+			d='M352 224c-11.28 0-21.95 2.305-32 5.879V224c0-35.3 28.7-64 64-64c17.67 0 32-14.33 32-32s-14.33-32-32-32c-70.58 0-128 57.42-128 128v96c0 53.02 42.98 96 96 96s96-42.98 96-96S405 224 352 224z'
+		/>
+	</svg>
+)
+
+export default QuoteLeftIcon

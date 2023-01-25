409724760f9af41caf23cb7e3b6c568a5a01fcb3
riolly <alberto.riolly@gmail.com>
2023-01-25 11:37:53 +0700

    :recycle: refactor: alternative input without provider

--- src/components/textarea-input.tsx
+++ src/components/textarea-input.tsx
@@ -4,6 +4,15 @@ import {ErrorMessage} from '@hookform/error-message'
 import {capFirstChar, slugify} from 'utils/literal'
 import cN from 'clsx'
 
+import {
+	type FieldValues,
+	type Path,
+	type UseFormRegister,
+	type FieldErrorsImpl,
+	type FieldName,
+} from 'react-hook-form'
+import {type FieldValuesFromFieldErrors} from '@hookform/error-message'
+
 type InputProps<T> = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
 	name: keyof T
 	label?: string
@@ -79,3 +88,74 @@ const TextAreaInput = <T,>({
 }
 
 export default TextAreaInput
+
+type Errors<T extends FieldValues> = Partial<FieldErrorsImpl<T>>
+
+type InputPropsNew<T extends FieldValues> =
+	React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+		name: Path<T>
+		register: UseFormRegister<T>
+		errors: Errors<T>
+		label?: string
+		wrapperClassName?: string
+		labelClassName?: string
+		inputClassName?: string
+		errorClassName?: string
+		autoGrow?: boolean
+	}
+
+export const TextAreaInputNew = <T extends FieldValues>({
+	name,
+	label,
+	register,
+	errors,
+	wrapperClassName,
+	labelClassName,
+	inputClassName = '',
+	errorClassName,
+	autoGrow = true,
+	...props
+}: InputPropsNew<T>) => {
+	React.useEffect(() => {
+		const textarea = document.querySelector(`#${name}`) as HTMLTextAreaElement
+		const resizeHeight = () => {
+			textarea.style.height = 'auto'
+			textarea.style.height = `${textarea.scrollHeight}px`
+		}
+
+		if (autoGrow) {
+			textarea.addEventListener('input', resizeHeight)
+		}
+		return () => {
+			textarea.removeEventListener('input', resizeHeight)
+		}
+	}, [name, autoGrow])
+
+	return (
+		<div className={cN('flex flex-col', wrapperClassName)}>
+			<label htmlFor={name} className={labelClassName}>
+				{label ?? capFirstChar(name)}
+			</label>
+			<textarea
+				id={name}
+				{...register(name)}
+				{...props}
+				className={cN(
+					'resize-none overflow-hidden rounded bg-light-bg/80 py-2 px-4',
+					inputClassName
+				)}
+			/>
+			<ErrorMessage
+				name={name as FieldName<FieldValuesFromFieldErrors<Errors<T>>>}
+				errors={errors}
+				render={({message}) => (
+					<small
+						className={cN('mt-0.5 font-medium text-red-500', errorClassName)}
+					>
+						{message}
+					</small>
+				)}
+			/>
+		</div>
+	)
+}
